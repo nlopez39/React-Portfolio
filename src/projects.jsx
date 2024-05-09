376176176ebc6865789d96e4d8d1ae3070e7f85c
Nelly Lopez <nellymlopez3@gmail.com>
2024-05-09 08:29:40 -0700

    switched out the project images

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,56 +1,50 @@
 export const projects = [
-    {
-        id: 1, 
-        name: "ANA-MARKET",
-        description: "Project 1",
-        demo : "https://ana-market-eae58deca7b8.herokuapp.com/",
-        repo: "https://github.com/nlopez39/ana-market",
-        img: "./images/code-1839406_1280.jpg",
-        
-    },
-    {
-        id: 2, 
-        name: "Weather App", 
-        description: "Projec t2",
-        demo:"https://nlopez39.github.io/Weather-App/",
-        repo: "https://github.com/nlopez39/Weather-App",
-        img:"./images/laptop-2620118_1280.jpg",
-    },
-    {
-        id: 3, 
-        name: "Coding Quiz",
-        description: "Project 3",
-        demo: "https://nlopez39.github.io/Coding-Quiz/",
-        repo: "https://github.com/nlopez39/Coding-Quiz",
-        img: "./images/code-1839406_1280.jpg",
-        
-    },
-    {
-        id: 4, 
-        name: "Password Generator",
-        description: "Project 4",
-        demo : "https://nlopez39.github.io/Password-Generator/",
-        repo: "https://github.com/nlopez39/Password-Generator",
-        img: "./images/interface-3614766_1280.png",
-        
-    },
-    {
-        id: 5, 
-        name: "Work Scheduler", 
-        description: "Project 5",
-        demo:"https://nlopez39.github.io/Work-Scheduler/",
-        repo: "https://github.com/nlopez39/Work-Scheduler",
-        img:"./images/laptop-2620118_1280.jpg",
-    },
-    {
-        id: 6, 
-        name: "Social Network API",
-        description: "Project 6",
-        demo: "https://drive.google.com/file/d/1Rx1QopFYpgGCXLUWBLD0mdSYhYSbfA7C/view",
-        repo: "https://github.com/nlopez39/Social-Network-API",
-        img: "./images/interface-3614766_1280.png",
-        
-    },
-
+  {
+    id: 1,
+    name: "ANA-MARKET",
+    description: "Project 1",
+    demo: "https://ana-market-eae58deca7b8.herokuapp.com/",
+    repo: "https://github.com/nlopez39/ana-market",
+    img: "./images/ana-market.png",
+  },
+  {
+    id: 2,
+    name: "Weather App",
+    description: "Projec t2",
+    demo: "https://nlopez39.github.io/Weather-App/",
+    repo: "https://github.com/nlopez39/Weather-App",
+    img: "./images/weather-app.png",
+  },
+  {
+    id: 3,
+    name: "Coding Quiz",
+    description: "Project 3",
+    demo: "https://nlopez39.github.io/Coding-Quiz/",
+    repo: "https://github.com/nlopez39/Coding-Quiz",
+    img: "./images/coding-quiz.png",
+  },
+  {
+    id: 4,
+    name: "Password Generator",
+    description: "Project 4",
+    demo: "https://nlopez39.github.io/Password-Generator/",
+    repo: "https://github.com/nlopez39/Password-Generator",
+    img: "./images/password-gen.png",
+  },
+  {
+    id: 5,
+    name: "Work Scheduler",
+    description: "Project 5",
+    demo: "https://nlopez39.github.io/Work-Scheduler/",
+    repo: "https://github.com/nlopez39/Work-Scheduler",
+    img: "./images/work-scheduler.png",
+  },
+  {
+    id: 6,
+    name: "Social Network API",
+    description: "Project 6",
+    demo: "https://drive.google.com/file/d/1Rx1QopFYpgGCXLUWBLD0mdSYhYSbfA7C/view",
+    repo: "https://github.com/nlopez39/Social-Network-API",
+    img: "./images/social-media.png",
+  },
 ];
-
