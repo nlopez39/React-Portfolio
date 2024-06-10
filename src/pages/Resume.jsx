f4ac4c4482e49a73b3c43324a97cabf1233a3083
Nelly Lopez <nellymlopez3@gmail.com>
2024-06-10 10:29:42 -0700

    updated resume link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -18,7 +18,7 @@ export default function Resume() {
       </ul>
       <p style={{ color: "#320A28" }}>
         Download my resume:{" "}
-        <a href="https://drive.google.com/file/d/1PpsKIoIFgiBRD-rxqB-Ls0s5gVWrPI9V/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/11T8lJxIQZLsMMf2MziBQXdxK5tEyL5xS/view?usp=sharing">
           Here!
         </a>
       </p>
